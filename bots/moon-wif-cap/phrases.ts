b3f9c4f5dfebc987a207f7e5d936bef6a492d8ef
joshwaiam <joshwaiam@gmail.com>
2022-08-28 21:15:37 -0400

    Tiny update to phrase

--- bots/moon-wif-cap/phrases.ts
+++ bots/moon-wif-cap/phrases.ts
@@ -3,7 +3,7 @@ export const blankSpot = '_____ '
 // Most of these sourced from https://linguazza.com/sentences-with/blank+spots
 export default [
   `I've got your ${blankSpot} right here.`,
-  `Oh, someone's got ${blankSpot} on the mind.`,
+  `Oh, someone's got a ${blankSpot} on the mind.`,
   `PAUSE.`,
   `Put your money where your ${blankSpot} is.`,
   `Mmmmm ${blankSpot}.`,
