6edb3ba94129721cf90799f0017421d960d8ec23
joshwaiam <joshwaiam@gmail.com>
2023-04-01 11:52:11 -0400

    Added more logic to Jordbot

--- bots/jordbot/index.ts
+++ bots/jordbot/index.ts
@@ -1,55 +1,37 @@
 import 'dotenv/config'
 import { Client, Intents } from 'discord.js'
-import { triggers, type Trigger } from './triggers'
+import { triggers, instruments, type Trigger } from './triggers'
 
 /**
  * Picks a random number between two numbers
- * @param min Lower bounds
- * @param max Upper bounds
- * @returns
  */
-function randomNumber(min: number, max: number) {
+function randomNumber(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-/** Discord.js client */
-const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-})
-
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-  console.info(`${new Date()} - Jordbot is ready!`)
-})
-
-// When a message is received, run this code
-client.on('messageCreate', async (message) => {
-  // Exit if message is from a bot
-  if (message.author.bot) return
-
-  /** Discord message content, lower-cased for better string matching. */
-  const content = message.content.toLowerCase()
-
-  // Check specific values
-  if (content.indexOf('miles davis') > -1) {
-    message.reply('https://imgur.com/a/FauNoJr')
-    return
+/**
+ * Checks if the message contains a one-off trigger.
+ */
+function checkOneOffTriggers(message: string): string | undefined {
+  if (message.indexOf('miles davis') > -1) {
+    return 'https://imgur.com/a/FauNoJr'
   }
 
-  if (content.indexOf('joey alexander') > -1) {
-    message.reply(
-      'For more info about Joey Alexander, greatest jazz pianist of our time, consult with Joey Alexander fan club president SquishyDough, and not some other imposter that rhymes with Bordo. https://i.guim.co.uk/img/media/6c45f0f6188c6b2ec1b357d74058588c00706c39/0_91_2696_1618/master/2696.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=ef8a45e1269f746a73aa5d322874121c'
-    )
-    return
+  if (message.indexOf('joey alexander') > -1) {
+    return 'For more info about Joey Alexander, greatest jazz pianist of our time, consult with Joey Alexander Fan Club president (aka Head Joey) SquishyDough, and not some other imposter that rhymes with Bordo. https://i.guim.co.uk/img/media/6c45f0f6188c6b2ec1b357d74058588c00706c39/0_91_2696_1618/master/2696.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=ef8a45e1269f746a73aa5d322874121c'
   }
+}
 
-  // Check all a matching artist triggers
-
-  /** The synonym found in the message content. */
+/**
+ * Checks if the message contains a name for a specific artist.
+ * If so, it will return a response stating a different artist who plays the
+ * same instrument is better, along with a link to their Wikipedia page.
+ */
+function checkArtistTriggers(message: string): string | undefined {
   let artistTrigger: Trigger | null = null
   for (const trigger of triggers) {
     const artist = trigger.artist.toLowerCase()
-    const artistFoundAt = content.indexOf(artist) // Returns the index where the word was found, -1 if not found
+    const artistFoundAt = message.indexOf(artist) // Returns the index where the word was found, -1 if not found
     const triggerFound = artistFoundAt > -1
 
     if (triggerFound) {
@@ -75,15 +57,72 @@ client.on('messageCreate', async (message) => {
     )}`
 
     // Send the response
-    message.reply(`
-        ${artistTrigger.artist} is a decent ${artistTrigger.instrument} player, but they're no ${similarTrigger.artist}. ${wikipediaUrl}
-      `)
+    return `${artistTrigger.artist} is a decent ${artistTrigger.instrument} player, but they're no ${similarTrigger.artist}. ${wikipediaUrl}`
+  }
+}
+
+function checkInstrumentTriggers(message: string): string | undefined {
+  let instrumentTrigger: Trigger | null = null
+  for (const trigger of instruments) {
+    const instrument = trigger.instrument.toLowerCase()
+    const instrumentFoundAt = message.indexOf(instrument) // Returns the index where the word was found, -1 if not found
+    const triggerFound = instrumentFoundAt > -1
+
+    if (triggerFound) {
+      instrumentTrigger = trigger
+      break // exit loop, don't need to look anymore
+    }
   }
 
-  // Add some emojis to the orginal message
-  // message.react('🍆')
-  // message.react('<:feelsMoonMan:980865025394745354>')
-  // message.react('<:smau:815016088576065547>')
+  // No action required if no trigger found
+  if (instrumentTrigger) {
+    // Find triggers from the same instrument type
+    const similarTriggers = instruments.filter(
+      (trigger) => trigger.instrument !== instrumentTrigger?.instrument
+    )
+    const similarTrigger =
+      similarTriggers[randomNumber(0, similarTriggers.length - 1)]
+
+    // Send the response
+    return `${instrumentTrigger.instrument} is a dogwater instrument. Try learning ${similarTrigger.instrument} if you want a real jazz instrument.`
+  }
+}
+
+/** Discord.js client */
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+})
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+  console.info(`${new Date()} - Jordbot is ready!`)
+})
+
+// When a message is received, run this code
+client.on('messageCreate', async (message) => {
+  // Exit if message is from a bot
+  if (message.author.bot) return
+
+  /** Discord message content, lower-cased for better string matching. */
+  const content = message.content.toLowerCase()
+
+  const oneOffResponse = checkOneOffTriggers(content)
+  if (oneOffResponse) {
+    message.reply(oneOffResponse)
+    return
+  }
+
+  const artistResponse = checkArtistTriggers(content)
+  if (artistResponse) {
+    message.reply(artistResponse)
+    return
+  }
+
+  const instrumentResponse = checkInstrumentTriggers(content)
+  if (instrumentResponse) {
+    message.reply(instrumentResponse)
+    return
+  }
 })
 
 // Login to Discord
