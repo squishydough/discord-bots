a0e5d96bd3a986d4c923390ca3f7aa8fc1f0d9ec
joshwaiam <joshwaiam@gmail.com>
2023-04-25 15:26:36 -0400

    Slightly increased random response weight

--- bots/jordbot/dist/index.d.ts
+++ bots/jordbot/dist/index.d.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-export declare const RANDOM_RESPONSE_WEIGHT = 3;
+export declare const RANDOM_RESPONSE_WEIGHT = 5;
 export declare const ARTIST_TRIGGER_WEIGHT = 20;
 export declare const INSTRUMENT_TRIGGER_WEIGHT = 20;
 //# sourceMappingURL=index.d.ts.map
--- bots/jordbot/dist/index.js
+++ bots/jordbot/dist/index.js
@@ -40,12 +40,11 @@ exports.INSTRUMENT_TRIGGER_WEIGHT = exports.ARTIST_TRIGGER_WEIGHT = exports.RAND
 require("dotenv/config");
 var discord_js_1 = require("discord.js");
 var triggers_1 = require("./triggers");
-// Sometimes we may not want a term to always trigger a response.
-// In those cases, we can use a weight to randomize whether it actually triggers.
-//
-// So if the weight is a number between 1 and 10, such as 6, it will trigger if a
-// random between 1 and 10 is 6 or less, i.e. a 60% chance.
-exports.RANDOM_RESPONSE_WEIGHT = 3;
+// Before returning a response, the bot will generate a random
+// number between 1 and 100. If the number is less than or equal
+// to the weight, the response will be returned. Otherwise, no
+// response will be returned.
+exports.RANDOM_RESPONSE_WEIGHT = 5;
 exports.ARTIST_TRIGGER_WEIGHT = 20;
 exports.INSTRUMENT_TRIGGER_WEIGHT = 20;
 /**
