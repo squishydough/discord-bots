a28fbc8a30c3a82d48bd7bed93c85f52bf17c49b
squishydough <hello@puggy.gg>
2023-08-16 11:26:15 -0400

    adjusted artist trigger response

--- bots/jordbot/dist/weights.d.ts
+++ bots/jordbot/dist/weights.d.ts
@@ -1,5 +1,5 @@
 export declare const RANDOM_RESPONSE_WEIGHT = 3;
-export declare const ARTIST_TRIGGER_WEIGHT = 15;
+export declare const ARTIST_TRIGGER_WEIGHT = 100;
 export declare const INSTRUMENT_TRIGGER_WEIGHT = 15;
 export declare const ONE_OFF_TRIGGER_WEIGHT = 15;
 //# sourceMappingURL=weights.d.ts.map
